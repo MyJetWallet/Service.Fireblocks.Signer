f01865dddcc04584e9984261ed38db8a297eb5be
Aleksandr B <aleksandr.b@smplt.net>
2022-01-19 21:30:28 +0400

    tools to init keys

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -19,29 +19,35 @@ namespace TestApp
             Console.ReadLine();
 
 
-            var factory = new FireblocksSignerClientFactory("http://localhost:5001");
-            //var factory = new FireblocksSignerClientFactory("http://fireblocks-signer.spot-services.svc.cluster.local");
+            //var factory = new FireblocksSignerClientFactory("http://localhost:5001");
+            var factory = new FireblocksSignerClientFactory("http://fireblocks-signer.spot-services.svc.cluster.local");
             var client = factory.GetTransactionService();
             var encryption = factory.GetEncryptionService();
 
             //var publicKey = await File.ReadAllTextAsync(@"D:\fireblocks uat\fireblocks_api_key");
             //var privateKey = await File.ReadAllTextAsync(@"D:\fireblocks uat\fireblocks_secret.key");
 
-            //var x = await encryption.SetApiKeysAsync(new ()
-            //{
-            //    ApiKey = publicKey,
-            //    PrivateKey = privateKey 
-            //});
+            var publicKey = args[0];
+            var privateKey = await File.ReadAllTextAsync(args[1]);
 
-            var tx = await client.CreateTransactionAsync(new Service.Fireblocks.Signer.Grpc.Models.Transactions.CreateTransactionRequest
+
+            var x = await encryption.SetApiKeysAsync(new()
             {
-                Amount = 0.01m,
-                AssetSymbol = "ETH",
-                AssetNetwork = "fireblocks-eth-test",
-                ExternalTransactionId = Guid.NewGuid().ToString(),
-                Tag = "",
-                ToAddress = "0x1Eab7d412a25a5d00Ec3d04648aa54CeA4aB7e94"
+                ApiKey = publicKey,
+                PrivateKey = privateKey
             });
+
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(x));
+
+            //var tx = await client.CreateTransactionAsync(new Service.Fireblocks.Signer.Grpc.Models.Transactions.CreateTransactionRequest
+            //{
+            //    Amount = 0.01m,
+            //    AssetSymbol = "ETH",
+            //    AssetNetwork = "fireblocks-eth-test",
+            //    ExternalTransactionId = Guid.NewGuid().ToString(),
+            //    Tag = "",
+            //    ToAddress = "0x1Eab7d412a25a5d00Ec3d04648aa54CeA4aB7e94"
+            //});
             //var resp = await client.GetVaultAccountAsync(new()
             //{
             //    VaultAccountId = "3"
